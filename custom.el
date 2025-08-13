98cf33b4abab62d3158f7dd4005e573f6d6219da
Arttu Siipola <arttu.siipola@helsinki.fi>
2025-08-13 09:45:23 +0300

    Separate customs

---
+++ custom.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ ;; If you edit by hand, keep only one custom-set-variables in your config.
+ '(custom-enabled-themes '(modus-operandi-tinted))
+ '(package-selected-packages '(magit))
+ '(scroll-bar-mode nil))                       ; hide GUI scrollbars
+
+(custom-set-faces
+ ;; If you edit by hand, keep only one custom-set-faces in your config.
+ '(default ((t (:family "Menlo" :slant normal :weight regular :height 240 :width normal)))))
